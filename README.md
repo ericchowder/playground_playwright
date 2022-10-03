41258338433790d328f240dd061a86093c9bb311
Eric Chau <eric.chau@zennioptical.com>
2022-10-03 10:13:21 -0400

    Added comments and readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Playground for playing with Playwright
 
 So much playing!
+
+### How To
+1. Clone repo, then run:
+```bash
+$ npm install
+```
+
+2. Run tests:
+```bash
+$ npx playwright test
+```
--- tests/main_page.spec.ts
+++ tests/main_page.spec.ts
@@ -1,7 +1,10 @@
 const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
+    // Go to url
     await page.goto('https://playwright.dev/');
+    // Get Selector
     const name = await page.innerText('.navbar__title');
+    // Assert Selector value
     expect(name).toBe('Playwright');
 })
